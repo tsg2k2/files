a3222677361f865f088c288f7a8ac0e6c4371e4f
tsg2k2 <tsg2k2@gmail.com>
2023-08-07 20:02:36 -0400

    data mount

--- lib/mmc/init.sh
+++ lib/mmc/init.sh
@@ -1,6 +1,5 @@
+#!/bin/ash
+data_mmcblk=$(grep -rn PARTNAME /sys/block/mmcblk0 | grep -w data | cut -d '/' -f 5)
 mkdir -p /mnt/data
-data_mmcblk=$(grep -rn PARTNAME sys/block/mmcblk0 | grep -w data | cut -d '/' -f 4)
-cryptsetup --key-file=/lib/mmc/data_key luksOpen /dev/$data_mmcblk data 2>&1 /dev/console
-
-
-
+cryptsetup --key-file=/lib/mmc/data_key luksOpen /dev/$data_mmcblk data
+mount /dev/mapper/data /mnt/data
